ed9698b81efc8211611822b2e258891036534cc9
Takayama Fumihiko <takayama.fumihiko@premo-inc.com>
2023-12-25 09:22:13 +0000

    Update XILINX_BOARD

--- fpga/pulpissimo-zedboard/fpga-settings.mk
+++ fpga/pulpissimo-zedboard/fpga-settings.mk
@@ -1,6 +1,6 @@
 export BOARD=zedboard
 export XILINX_PART=xc7z020clg484-1
-export XILINX_BOARD=em.avnet.com:zed:part0:1.4
+export XILINX_BOARD=avnet.com:zedboard:part0:1.4
 export FC_CLK_PERIOD_NS=62.5
 export PER_CLK_PERIOD_NS=100
 export SLOW_CLK_PERIOD_NS=30517
